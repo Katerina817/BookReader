adde9e12d3f7f8a202583210b154f190b5b569ff
Firisyuk Katerina <firisyuk.e.a@edu.mirea.ru>
2026-01-03 00:31:25 +0300

    Initial commit

---
+++ src/main/java/com/example/bookreader/BookReaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReaderApplication.class, args);
+	}
+
+}
