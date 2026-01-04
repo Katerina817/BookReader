f036ac5d290114a935a94ac7ff53dbfe388d57e9
Firisyuk Katerina <firisyuk.e.a@edu.mirea.ru>
2026-01-04 18:10:49 +0300

    Add User entity with id, login,password, email

---
+++ src/main/java/com/example/bookreader/entity/User.java
@@ -0,0 +1,56 @@
+package com.example.bookreader.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 20)
+    private String password;
+
+    @Column(nullable = false,unique = true,length = 50)
+    private String login;
+
+    @Column(unique = true,length = 150)
+    private String email;
+
+    public User(){
+    }
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
