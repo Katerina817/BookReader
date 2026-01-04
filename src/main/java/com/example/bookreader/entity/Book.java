c68f455702e325318d795874e1b917e968f4712e
Firisyuk Katerina <firisyuk.e.a@edu.mirea.ru>
2026-01-04 18:11:15 +0300

    Add Book and Genre entities with ManyToMany relation

---
+++ src/main/java/com/example/bookreader/entity/Book.java
@@ -0,0 +1,74 @@
+package com.example.bookreader.entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String author;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    public Book() {}
+
+    @ManyToMany
+    @JoinTable(
+         name="book_genre",
+         joinColumns = @JoinColumn(name ="book_id"),
+         inverseJoinColumns =@JoinColumn(name = "genre_id")
+    )
+    private Set<Genre> genres=new HashSet<>();
+
+    public Set<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(Set<Genre> genres) {
+        this.genres = genres;
+    }
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/example/bookreader/entity/Genre.java
@@ -0,0 +1,35 @@
+package com.example.bookreader.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "genres")
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    public Genre() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
