3ce031ecb6eb729442d05134b288a08ee3d031d2
Firisyuk Katerina <firisyuk.e.a@edu.mirea.ru>
2026-01-04 18:11:48 +0300

    Add Note entity linked to Reading

---
+++ src/main/java/com/example/bookreader/entity/Note.java
@@ -0,0 +1,61 @@
+package com.example.bookreader.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "notes")
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(columnDefinition = "text",nullable = false)
+    private String content;
+
+    private LocalDateTime dateOfCreation;
+    @PrePersist
+    protected void onCreate() {
+        dateOfCreation = LocalDateTime.now();
+    }
+    @ManyToOne
+    @JoinColumn(name = "reading_id", nullable = false)
+    private Reading reading;
+
+    public Note() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getDateOfCreation() {
+        return dateOfCreation;
+    }
+
+    public void setDateOfCreation(LocalDateTime dateOfCreation) {
+        this.dateOfCreation = dateOfCreation;
+    }
+
+    public Reading getReading() {
+        return reading;
+    }
+
+    public void setReading(Reading reading) {
+        this.reading = reading;
+    }
+}
