3f257bc43e607681d51789f8f090c0f7437e5f48
Firisyuk Katerina <firisyuk.e.a@edu.mirea.ru>
2026-01-20 16:56:45 +0300

    add NoteController + add NoteMapper

---
+++ src/main/java/com/example/bookreader/controller/NoteController.java
@@ -0,0 +1,58 @@
+package com.example.bookreader.controller;
+
+import com.example.bookreader.DTO.NoteControllerDTO.CreateNoteRequest;
+import com.example.bookreader.DTO.NoteControllerDTO.NoteResponse;
+import com.example.bookreader.DTO.NoteControllerDTO.UpdateNoteRequest;
+import com.example.bookreader.mapper.NoteMapper;
+import com.example.bookreader.entity.Note;
+import com.example.bookreader.service.NoteService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/readings/{readingId}/notes")
+public class NoteController {
+    private final NoteService noteService;
+
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @PostMapping
+    public NoteResponse createNote(
+            @PathVariable UUID readingId,
+            @RequestBody @Valid CreateNoteRequest request) {
+        Note note = noteService.createNote(readingId, request.getContent());
+        return NoteMapper.toResponse(note);
+    }
+
+    @DeleteMapping("/{noteId}")
+    public void deleteNote(
+            @PathVariable UUID readingId,
+            @PathVariable UUID noteId) {
+        noteService.deleteNote(readingId, noteId);
+    }
+
+    @PutMapping("/{noteId}")
+    public NoteResponse updateNote(
+            @PathVariable UUID readingId,
+            @PathVariable UUID noteId,
+            @RequestBody @Valid UpdateNoteRequest request){
+        Note note=noteService.updateNote(readingId,noteId,request.getContent());
+        return NoteMapper.toResponse(note);
+    }
+
+    @GetMapping
+    public List<NoteResponse> getAllNotes(
+            @PathVariable UUID readingId
+    ){
+        List<Note>notes=noteService.getNotesByReading(readingId);
+        return notes.stream()
+                .map(NoteMapper::toResponse)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/example/bookreader/mapper/NoteMapper.java
@@ -0,0 +1,15 @@
+package com.example.bookreader.mapper;
+
+import com.example.bookreader.DTO.NoteControllerDTO.NoteResponse;
+import com.example.bookreader.entity.Note;
+
+public class NoteMapper {
+    public static NoteResponse toResponse(Note note) {
+        NoteResponse response = new NoteResponse();
+        response.setId(note.getId());
+        response.setContent(note.getContent());
+        response.setReadingId(note.getReading().getId());
+        response.setDateOfCreation(note.getDateOfCreation());
+        return response;
+    }
+}
